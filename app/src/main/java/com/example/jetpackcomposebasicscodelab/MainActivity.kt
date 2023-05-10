24c3d9a7a94e2e031f6184a7b6552625e07fe60b
gogumaC(mac mini) <kimu2371@naver.com>
2023-05-11 02:39:08 +0900

    feat: Make same padding text box by Modifier.padding and fillMaxWidth

--- app/src/main/java/com/example/jetpackcomposebasicscodelab/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposebasicscodelab/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -35,7 +36,7 @@ private fun MyApp(
         modifier = modifier,
         color = MaterialTheme.colorScheme.background
     ) {
-        Column{
+        Column(modifier = Modifier.padding(vertical=4.dp)){
             for(name in names){
                 Greeting(name)
             }
@@ -45,8 +46,11 @@ private fun MyApp(
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Surface(color = MaterialTheme.colorScheme.primary) {
-        Column(modifier= Modifier.padding(24.dp)){
+    Surface(
+        color = MaterialTheme.colorScheme.primary,
+        modifier = Modifier.padding(vertical=4.dp, horizontal = 8.dp)
+    ) {
+        Column(modifier= Modifier.padding(24.dp).fillMaxWidth()){
             Text(text = "Hello,")
             Text(text = name)
         }
