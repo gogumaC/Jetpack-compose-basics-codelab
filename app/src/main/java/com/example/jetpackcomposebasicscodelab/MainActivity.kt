daab856208b7489c13296269a1929d08895e07d0
gogumaC(mac mini) <kimu2371@naver.com>
2023-05-11 02:47:30 +0900

    feat: Add ElevatedButton

--- app/src/main/java/com/example/jetpackcomposebasicscodelab/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposebasicscodelab/MainActivity.kt
@@ -4,9 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -50,10 +53,18 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         color = MaterialTheme.colorScheme.primary,
         modifier = Modifier.padding(vertical=4.dp, horizontal = 8.dp)
     ) {
-        Column(modifier= Modifier.padding(24.dp).fillMaxWidth()){
-            Text(text = "Hello,")
-            Text(text = name)
+        Row(modifier = Modifier.padding(24.dp).fillMaxWidth()){
+            Column(modifier= Modifier.weight(1f)
+            ){
+                Text(text = "Hello,")
+                Text(text = name)
+            }
+            ElevatedButton(onClick = {}) {
+                Text(text="Show more")
+            }
+
         }
+
 
     }
 }
