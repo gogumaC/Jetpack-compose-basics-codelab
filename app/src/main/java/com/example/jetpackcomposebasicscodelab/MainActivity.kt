8d9145cfcc6635ad112370ac5cf040b4ad099a46
gogumaC(mac mini) <kimu2371@naver.com>
2023-05-11 17:48:55 +0900

    feat: Implement OnboardingScreen(state hoisting)

--- app/src/main/java/com/example/jetpackcomposebasicscodelab/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposebasicscodelab/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.jetpackcomposebasicscodelab
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -16,10 +17,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.jetpackcomposebasicscodelab.ui.theme.JetpackComposeBasicsCodelabTheme
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -35,17 +39,40 @@ class MainActivity : ComponentActivity() {
 @Composable
 private fun MyApp(
     modifier: Modifier = Modifier,
-    names:List<String> = listOf("World","Compose")
 ) {
+    var shouldShowOnboarding by remember{mutableStateOf(true)}
     Surface(
-        modifier = modifier,
-        color = MaterialTheme.colorScheme.background
+        modifier = modifier
     ) {
-        Column(modifier = Modifier.padding(vertical=4.dp)){
-            for(name in names){
-                Greeting(name)
-            }
+        if(shouldShowOnboarding)OnboardingScreen(onContinueClicked={shouldShowOnboarding=false})
+        else Greetings()
+    }
+}
+
+@Preview
+@Composable
+fun MyAppPreview() {
+    JetpackComposeBasicsCodelabTheme {
+        MyApp(Modifier.fillMaxSize())
+    }
+}
+
+@Composable
+private fun Greetings(
+    modifier: Modifier=Modifier,
+    names:List<String> =listOf("World","Compose")
+){
+    Column(modifier = modifier.padding(vertical=4.dp)){
+        for(name in names){
+            Greeting(name)
         }
+    }
+}
+@Preview(showBackground = true, widthDp = 320)
+@Composable
+private fun GreetingsPreview(){
+    JetpackComposeBasicsCodelabTheme {
+        Greetings()
     }
 }
 
@@ -57,9 +84,12 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         color = MaterialTheme.colorScheme.primary,
         modifier = Modifier.padding(vertical=4.dp, horizontal = 8.dp)
     ) {
-        Row(modifier = Modifier.padding(24.dp).fillMaxWidth()){
-            Column(modifier= Modifier.weight(1f)
-                .padding(bottom=extraPadding)
+        Row(modifier = Modifier
+            .padding(24.dp)
+            .fillMaxWidth()){
+            Column(modifier= Modifier
+                .weight(1f)
+                .padding(bottom = extraPadding)
             ){
                 Text(text = "Hello,")
                 Text(text = name)
@@ -77,5 +107,33 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 fun GreetingPreview() {
     JetpackComposeBasicsCodelabTheme {
         MyApp()
+    }
+}
+
+@Composable
+fun OnboardingScreen(
+    modifier: Modifier=Modifier,
+    onContinueClicked:()->Unit
+){
+    Column(
+        modifier=modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Text("Welcome to the Basics Codelab!")
+        Button(
+            modifier=Modifier.padding(vertical=24.dp),
+            onClick = onContinueClicked
+        ){
+            Text("Continue")
+        }
+    }
+}
+
+@Preview(showBackground=true,widthDp=320, heightDp = 320)
+@Composable
+fun OnboardingPreview(){
+    JetpackComposeBasicsCodelabTheme {
+        OnboardingScreen(onContinueClicked = {})
     }
 }
